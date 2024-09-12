f5bb731b0138b8cd4b4a40d11010e3cc4cbba517
Manoj Sundar Ravichandran <manojsundar187@gmail.com>
2024-09-13 01:57:29 +0200

    src commit

--- setup.py
+++ setup.py
@@ -10,9 +10,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file:
         requirements=file.readlines()
-        requirements=[req.replace('\n','')for req in requirements]
-            
-        
+        requirements=[req.replace('\n','')for req in requirements] 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
@@ -21,7 +19,7 @@ setup(
  version='0.0.1',
  author='manoj_sundar_ravichandran',
  author_email='manojsundar187@gmail.com',
- packages=find_packages(),
+ packages=find_packages(),   # check how many files have __init__.py file 
  install_requires=get_requirements('requirement.txt')
 )
    
